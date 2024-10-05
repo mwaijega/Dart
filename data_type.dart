a8f2a681faa8b7336a33e1336b8971200f33349c
kelvin mwaijega <130301357+ministerko@users.noreply.github.com>
2024-10-05 16:04:17 +0300

    add data types

---
+++ data_type.dart
@@ -0,0 +1,24 @@
+void main() {
+  /*
+    int
+    double
+    String
+    bool
+    dynamic
+   */
+  var amount1 = 100;
+  var amount2 =200;
+  print('Amount1: $amount1 | Amount2:$amount2\n');
+
+
+
+  double amount3 = 180.4;
+  var amount4 = 200.5;
+  print('Amount3: $amount3 | Amount4 $amount4');
+
+  dynamic amount5 = 5000;
+  print ('Amount5: $amount5');
+
+  amount5 = "elfu tano";
+  print('Amount5:$amount5');
+}
