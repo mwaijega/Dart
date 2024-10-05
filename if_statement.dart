ddd4d057e9f12bbea996f8de4ef6ab845c14c7a7
kelvin mwaijega <130301357+ministerko@users.noreply.github.com>
2024-10-05 22:24:53 +0300

    conditional statement

---
+++ if_statement.dart
@@ -0,0 +1,13 @@
+void main(){
+  int number = 30;
+
+  if(number%2==0){
+    print('Even');
+  }
+  else if(number%3 ==0){
+    print('Odd');
+  }
+  else{
+    print('Confused');
+  }
+}
---
+++ switch_statement.dart
@@ -0,0 +1,18 @@
+void main(){
+  int number = 0;
+
+  switch(number){
+    case 0:
+      print("Number is zero");
+      break;
+    case 1:
+      print("Number is one");
+      break;
+    case 2:
+      print("Number is two");
+      break;
+    default:
+      print("Number is not valid");
+      
+  }
+}
