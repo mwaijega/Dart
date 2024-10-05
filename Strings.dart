8fe87b332bd9fc4abd71e9d27f5fd3fdc9b8f2c4
kelvin mwaijega <130301357+ministerko@users.noreply.github.com>
2024-10-05 16:41:27 +0300

    strings

---
+++ Strings.dart
@@ -0,0 +1,16 @@
+main(){
+  var s1 = 'Hello';
+  var s2 ="Hello";
+  var s3 ='It\'s Hello';
+  var s4 = "It's Hello";
+
+  print(s1);
+  print(s2);
+  print(s3);
+  print(s4);
+  print('');
+
+  var s = r'Hello';
+  print(s);
+
+}
--- data_type.dart
+++ data_type.dart
@@ -6,7 +6,7 @@ void main() {
     bool
     dynamic
    */
-  var amount1 = 100;
+  var amount1 = 100; 
   var amount2 =200;
   print('Amount1: $amount1 | Amount2:$amount2\n');
 
