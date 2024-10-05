c34d3d0291697e1a95e8e75c800b69cca0a996ee
kelvin mwaijega <130301357+ministerko@users.noreply.github.com>
2024-10-05 20:14:29 +0300

    constant

---
+++ constant.dart
@@ -0,0 +1,14 @@
+main(){
+  const aConstNum = 0;
+  const aConstBool = true;
+  const aConstString = "Hello World!";
+
+  print(aConstNum);
+  print(aConstBool);
+  print(aConstString);
+
+  print(aConstNum.runtimeType);
+  print(aConstBool.runtimeType);
+  print(aConstString.runtimeType);
+}
+
